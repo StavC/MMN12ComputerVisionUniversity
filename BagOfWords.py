05c518ba76284a5aae2f30ba997c5f42f5ea6dde
Stav <cohenstav1@gmail.com>
2020-11-18 17:28:58 +0200

    Starting to Work on part 3 BagOfWords Classification

--- BagOfWords.py
+++ BagOfWords.py
@@ -34,6 +34,56 @@ def main2():
                 street.append(os.path.join(r, file))
             if 'tallbuilding' in file:
                 tallbuilding.append(os.path.join(r, file))
+    train_x=[]
+    train_y=[]
+    test_x=[]
+    test_y=[]
+    minPictures=(min(len(coast),len(forest),len(highway),len(insidecity),len(mountain),len(opencountry),len(street),len(tallbuilding)))
+    print(f'the least amount of pictures from all of the categories is : {minPictures}')
+    ratio_list=[0.8]
+    for ratios in ratio_list:
+        ratio=int(minPictures*ratios) # im balancing the dataset so each class will have equal amount of pictures to avoid overfit!
+        #for i in range (ratio):
+        for i in range (minPictures):
+            if i< ratio:
+
+                train_x.append(coast[i])
+                train_y.append('coast')
+                train_x.append(forest[i])
+                train_y.append('forest')
+                train_x.append(highway[i])
+                train_y.append('highway')
+                train_x.append(insidecity[i])
+                train_y.append('insidecity')
+                train_x.append(mountain[i])
+                train_y.append('mountain')
+                train_x.append(opencountry[i])
+                train_y.append('opencountry')
+                train_x.append(street[i])
+                train_y.append('street')
+                train_x.append(tallbuilding[i])
+                train_y.append('tallbuilding')
+            else:
+                test_x.append(coast[i])
+                test_y.append('coast')
+                test_x.append(forest[i])
+                test_y.append('forest')
+                test_x.append(highway[i])
+                test_y.append('highway')
+                test_x.append(insidecity[i])
+                test_y.append('insidecity')
+                test_x.append(mountain[i])
+                test_y.append('mountain')
+                test_x.append(opencountry[i])
+                test_y.append('opencountry')
+                test_x.append(street[i])
+                test_y.append('street')
+                test_x.append(tallbuilding[i])
+                test_y.append('tallbuilding')
+
+
+        print(f'train_x and train_y sizes are:  {len(train_x)},{len(train_y)}') #making sure the math done right and we got 260*8*0.8=1664 pictures in the training set
+        print(f'test_x and test_y sizes are:  {len(test_x)},{len(test_y)}') #making sure the math done right and we got 260*8*0.2=416 pictures in the testing set
 
 
 
